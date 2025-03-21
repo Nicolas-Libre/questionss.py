9cdc363afa369fd4b2411733ee450db51c2b98f2
Nicolas-Libre <nicolaslibre22@gmail.com>
2025-03-21 14:17:45 -0300

    Primera consigna de la entrega

--- questions.py
+++ questions.py
@@ -1,5 +1,6 @@
 import random
 # Preguntas para el juego
+
 questions = [
     "¿Qué función se usa para obtener la longitud de una cadena en Python?",
     "¿Cuál de las siguientes opciones es un número entero en Python?",
@@ -7,8 +8,8 @@ questions = [
     "¿Cuál de las siguientes expresiones es un comentario válido en Python?",
     "¿Cuál es el operador de comparación para verificar si dos valores son iguales?"
 ]
-# Respuestas posibles para cada pregunta, en el mismo orden
-#que las preguntas
+
+# Respuestas posibles para cada pregunta, en el mismo orden que las preguntas
 answers = [
     ("size()", "len()", "length()", "count()"),
     ("3.14", "'42'", "10", "True"),
@@ -21,29 +22,43 @@ answers = [
     ),
     ("=", "==", "!=", "==="),
 ]
-# Índice de la respuesta correcta para cada pregunta, el el
-#mismo orden que las preguntas
+
+# Índice de la respuesta correcta para cada pregunta, en el mismo orden que las preguntas
 correct_answers_index = [1, 2, 0, 3, 1]
 
 # El usuario deberá contestar 3 preguntas
 for _ in range(3):
+
     # Se selecciona una pregunta aleatoria
     question_index = random.randint(0, len(questions) - 1)
+
     # Se muestra la pregunta y las respuestas posibles
     print(questions[question_index])
     for i, answer in enumerate(answers[question_index]):
         print(f"{i + 1}. {answer}")
+
 # El usuario tiene 2 intentos para responder correctamente
     for intento in range(2):
-        user_answer = int(input("Respuesta: ")) - 1
+        user_answer = (input("Respuesta: ")) 
+
+        #Si la respuesta no es valida, se termina el programa
+        if user_answer < "1" or user_answer > "4":
+            print ("Respuesta no valida")
+            exit(1)
+
 # Se verifica si la respuesta es correcta
-        if user_answer == correct_answers_index[question_index]:
+        if int(user_answer)-1 == correct_answers_index[question_index]:
             print("¡Correcto!")
             break
     else:
+
         # Si el usuario no responde correctamente después de 2 intentos,
         # se muestra la respuesta correcta
         print("Incorrecto. La respuesta correcta es:")
         print(answers[question_index] [correct_answers_index[question_index]])
+
 # Se imprime un blanco al final de la pregunta
     print()
+    
+
+
